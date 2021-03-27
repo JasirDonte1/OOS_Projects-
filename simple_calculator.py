dda2ffe9ec442a766e956241179dce0285c71e91
JasirDonte1 <33403498+JasirDonte1@users.noreply.github.com>
2021-03-27 13:12:07 -0400

    Update simple_calculator.py

--- simple_calculator.py
+++ simple_calculator.py
@@ -38,25 +38,23 @@ operations = ["+","-","/","*"]  #operations array
 counter = 0 #iterator to track the proper operation performed.
 
 
-x = user_input.split(operations[counter])
-while (len(x) < 2):       #will iterate throught operations until correct operation is found/performed
-    counter = counter + 1
-    x = user_input.split(operations[counter])
+x = user_input.split(operations[counter])  #primes loop to find correct operation to be performed. Split will occur when operation symbol from user input matches symbol in operations array
+while (len(x) < 2):       #will iterate throught operations until correct operation is found
+    counter = counter + 1  #tracks which operation was performed 
+    x = user_input.split(operations[counter])     #split creates an array holding value 1 and value 2 in slots 0 and 1 in the array 
 
 
 calculator_1 = Calculator()     #instantiate new Calculator object 
 
 '''
-
 If else statement determines which operation should be performed based on the value of the counter
 variable. Counter value corresponds to the operations place in the "operations" array.
 
 y = the calcualted answer to the arithmatic problem
-
 '''
 
-if(counter == 0):
-    y = calculator_1.findSum(float(x[0]), float(x[1]))
+if(counter == 0):  
+    y = calculator_1.findSum(float(x[0]), float(x[1])) #wraps string values to be conveted to floats 
 elif(counter == 1):
     y = calculator_1.findDifference(float(x[0]), float(x[1]))
 elif(counter == 2):
@@ -67,6 +65,6 @@ elif(counter == 3):
 
 #print statement prints orginal arithmetic statement and calculated value from class method    
 
-print(user_input + " = " + str(y))
+print(user_input + " = " + str(y)) # str() converts string back in to float 
     
 
